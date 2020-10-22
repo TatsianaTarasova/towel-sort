3c2ec620cc65dc4ff56da3ed224d720778dd08db
TatsianaTarasova <70853063+TatsianaTarasova@users.noreply.github.com>
2020-10-22 10:58:10 +0200

    init : result tak towersort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+let Arr = [];
+if(matrix === [] || ! matrix) {
   return [];
+}
+for(let i = 0; i < matrix.length; i++){
+  if(i % 2 !== 0){
+    Arr.push(matrix[i].reverse());
+  } 
+  else{
+    Arr.push(matrix[i]);
+  }
+}
+return Arr.flat();
 }
